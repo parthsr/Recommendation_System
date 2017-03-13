2bbdbe9624424447010fff8c1147e751244b35ca
Parth Srivastava <srivastavaparth2016@gmail.com>
2017-03-13 18:05:05 +0530

    files for kmeans clustering

--- app.py
+++ app.py
@@ -1,149 +1,143 @@
-
-
-#########################################
-# define our feeds
-#########################################
-feeds = [
-            
-        'http://feeds.reuters.com/reuters/INworldNews',
-    'http://feeds.reuters.com/reuters/INtechnologyNews',
-    'http://feeds.reuters.com/reuters/INhealth',
-    'http://feeds.reuters.com/reuters/INlifestyle',
-    'http://feeds.reuters.com/reuters/INhollywood',
-    
-]
-
-#########################################
-# parse the feeds into a set of words per document
-#########################################
-import feedparser
+import numpy as np
+import pandas as pd
+import re
+import os
+import codecs
 import nltk
-from bs4 import BeautifulSoup
-corpus = []
-titles=[]
-ct = -1
+from sklearn import feature_extraction
+import mpld3
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
+
+i=0
+find_d="find_d"
+find_t="find_t"
+file = open('test.txt', 'r')
+
+for line in file:
+    if i==0:
+        find_t=line
+        i+=1
+    else:
+        find_d=line
+
+print find_t
+print "......"
+print find_d
+
+''''nltk.download("stopwords")
+nltk.download('punkt')'''
+stemmer = SnowballStemmer('english')
+stopWords = set(stopwords.words('english'))
+
+# here I define a tokenizer and stemmer which returns the set of stems in the text that it is passed
+
+def tokenize_and_stem(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
 
 
-for feed in feeds:
-    d = feedparser.parse(feed)
-    for e in d['entries']:
-       soup = BeautifulSoup(e['description'])#https://www.crummy.com/software/BeautifulSoup/bs4/doc/#porting-code-to-bs4
-       text = soup.get_text() # a description
-       words = nltk.wordpunct_tokenize(text) #tokenized description
-       words.extend(nltk.wordpunct_tokenize(e['title'])) #tokenized description with tokenized title 
-       lowerwords=[x.lower() for x in words if len(x) > 1]
-       ct += 1
-       print ct, "TITLE",e['title']
-       corpus.append(lowerwords)
-       titles.append(e['title'])
+def tokenize_only(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    return filtered_tokens
 
-#########################################
-# tf-idf implementation
 
-# from http://timtrueman.com/a-quick-foray-into-linear-algebra-and-python-tf-idf/
-#########################################
-import math
-from operator import itemgetter
-def freq(word, document): return document.count(word)
-def wordCount(document): return len(document)
-def numDocsContaining(word,documentList):
-  count = 0
-  for document in documentList:
-    if freq(word,document) > 0:
-      count += 1
-  return count
-def tf(word, document): return (freq(word,document) / float(wordCount(document)))
-def idf(word, documentList): return math.log(len(documentList) / numDocsContaining(word,documentList))
-def tfidf(word, document, documentList): return (tf(word,document) * idf(word,documentList))
+    
+def readFile2():
+    with open('hello2.txt') as f:
+        titles = f.read().decode('utf-8').strip().splitlines()
+        titles.append(find_t)
+    return titles
 
-#########################################
-# extract top keywords from each doc.
-# This defines features of our common feature vector
-#########################################
-import operator
-def top_keywords(n,doc,corpus):
-    d = {}
-    for word in set(doc):
-        d[word] = tfidf(word,doc,corpus)
-    sorted_d = sorted(d.iteritems(), key=operator.itemgetter(1))
-    sorted_d.reverse()
-    return [w[0] for w in sorted_d[:n]]   
 
-key_word_list=set()
-nkeywords=4
-[[key_word_list.add(x) for x in top_keywords(nkeywords,doc,corpus)] for doc in corpus]
-   
-ct=-1
-for doc in corpus:
-   ct+=1
-   print ct,"KEYWORDS"," ".join(top_keywords(nkeywords,doc,corpus))
+#tfidf_transformer=getAlgorithms();
 
-#########################################
-# turn each doc into a feature vector using TF-IDF score
-#########################################
-feature_vectors=[]
-n=len(corpus)
 
-for document in corpus:
-    vec=[]
-    [vec.append(tfidf(word, document, corpus) if word in document else 0) for word in key_word_list]
-    feature_vectors.append(vec)#getting the feature words in the array-parth
+with open('hello.txt') as f:
+    lines = f.read().decode('utf-8').strip().splitlines()
+    lines.append(find_d)
+totalvocab_stemmed = []
+totalvocab_tokenized = []
+for i in lines:
+    allwords_stemmed = tokenize_and_stem(i) #for each item in 'synopses', tokenize/stem
+    totalvocab_stemmed.extend(allwords_stemmed) #extend the 'totalvocab_stemmed' list
+    
+    allwords_tokenized = tokenize_only(i)
+    totalvocab_tokenized.extend(allwords_tokenized)
 
-#########################################
-# now turn that into symmatrix matrix of 
-# cosine similarities
-#########################################
-import numpy
-mat = numpy.empty((n, n))
-for i in xrange(0,n):
-    for j in xrange(0,n):
-       mat[i][j] = nltk.cluster.util.cosine_distance(feature_vectors[i],feature_vectors[j])#evaluating the cosine similarity of each feature vector with other feature vectors
+vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index = totalvocab_stemmed)
+print 'there are ' + str(vocab_frame.shape[0]) + ' items in vocab_frame'
 
-#########################################
-# now hierarchically cluster mat
-#########################################
-import matplotlib
-matplotlib.use('Agg') 
-from hcluster import linkage, dendrogram
-t =1.4
-Z = linkage(mat, 'complete')
-print Z
-dendrogram(Z, color_threshold=t)
+print vocab_frame.head()
 
-import pylab
-pylab.savefig( "hcluster.png" ,dpi=800)
+print
+print 
+print
 
-#########################################
-# extract our clusters
-#########################################
-def extract_clusters(Z,threshold,n):
-   clusters={}
-   ct=n
-   for row in Z:
-      if row[2] < threshold:
-          n1=int(row[0])
-          n2=int(row[1])
 
-          if n1 >= n:
-             l1=clusters[n1] 
-             del(clusters[n1]) 
-          else:
-             l1= [n1]
-      
-          if n2 >= n:
-             l2=clusters[n2] 
-             del(clusters[n2]) 
-          else:
-             l2= [n2]    
-          l1.extend(l2)  
-          clusters[ct] = l1
-          ct += 1
-      else:
-          return clusters
+tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
+                                 min_df=0.05, stop_words='english',
+                                 use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1,3))
+tfidf_matrix = tfidf_vectorizer.fit_transform(lines)
+'''print(tfidf_matrix.shape)'''
+terms = tfidf_vectorizer.get_feature_names()
+print terms
 
-clusters = extract_clusters(Z,t,n)
- 
-for key in clusters:
-   print "============================================="	
-   for id in clusters[key]:
-       print id,titles[id]
+
+
+
+dist = 1 - cosine_similarity(tfidf_matrix)
+print
+print
+
+
+
+num_clusters = 20
+km = KMeans(n_clusters=num_clusters)
+km.fit(tfidf_matrix)
+clusters = km.labels_.tolist()
+
+
+
+joblib.dump(km,  'doc_cluster.pkl')
+
+km = joblib.load('doc_cluster.pkl')
+clusters = km.labels_.tolist()
+
+films = {  'title':readFile2(),'description': lines, 'cluster': clusters }
+
+frame = pd.DataFrame(films, index = [clusters] , columns = ['title','description', 'cluster'])
+frame['cluster'].value_counts()
+
+
+print("Top terms per cluster:")
+print
+#sort cluster centers by proximity to centroid
+order_centroids = km.cluster_centers_.argsort()[:, ::-1] 
+
+for i in range(num_clusters):
+    print "Cluster "+ str(i)+ " words:" 
+    for ind in order_centroids[i, :3]: #replace 6 with n words per cluster
+        print vocab_frame.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore')
+    print #add whitespace
+    print #add whitespace
+    print "Cluster " +str(i)+" titles:"  
+    for desc in frame.ix[i]['title'].values.tolist():
+        print desc
